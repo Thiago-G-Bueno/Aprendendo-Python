2b8f1f2492cda89883d93357611e0844d4bc16c6
Thiago Bueno <thiago143310@gmail.com>
2025-04-19 18:37:28 -0300

    vai commit

--- Faculdade_Univille/Algortimos_Lógica_Programação/if_elif_else.py
+++ Faculdade_Univille/Algortimos_Lógica_Programação/if_elif_else.py
@@ -1,10 +1,7 @@
 a = 1
-b = 0
+b = 1
  
 if a and b:
     print ("isso é verdadeiro")
-elif a + b:
-    soma = a + b 
-    print (f"a soma de a e b é {soma}")
 else:
     print("isso não é verdadeiro")
