0b18c63ef56bb02254deb319f9c707f8f86b43f3
Thiago Bueno <thiago143310@gmail.com>
2025-04-22 17:38:28 -0300

    criando aquivo para anotar os principais comandos de git e github

---
+++ Aula_Udemy/Comandos.Git_Github.py
@@ -0,0 +1,12 @@
+
+
+#Passo a passo para subir alterações (via terminal integrado do VS Code):
+
+#git status
+
+#git add .
+
+#git commit -m "Descreva aqui a alteração feita"
+
+#git push origin main
+
